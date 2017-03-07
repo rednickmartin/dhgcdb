f4c9acc4b560f771df5099cf0cb5e09dcdd489d9
Nick Martin <nmartin@factset.com>
2017-03-07 19:14:32 +0000

    Further preparations for initial migration
    Removing default connection - it is not used

--- DHGCDB/Models/IdentityModels.cs
+++ DHGCDB/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace DHGCDB.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ClientDBContext", throwIfV1Schema: false)
         {
         }
 
