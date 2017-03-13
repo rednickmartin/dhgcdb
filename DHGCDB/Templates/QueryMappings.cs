f119e2b499d70fb98e9d221309c8d9078465b348
Nick Martin <nmartin@factset.com>
2017-03-13 09:00:34 +0000

    Adding report date to template

--- DHGCDB/Templates/QueryMappings.cs
+++ DHGCDB/Templates/QueryMappings.cs
@@ -9,6 +9,21 @@ namespace DHGCDB.Templates
 {
   public class QueryMappings
   {
+    private static string GetSuffix(int day)
+    {
+      var lsd = day % 10;
+      switch (lsd) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+      }
+    }
+
     public static Dictionary<string, Func<Review, string>> ReviewData = new Dictionary<string, Func<Review, string>> {
       {
         "CLIENTNAME_ADDRESS", (Review review) => {
@@ -33,6 +48,15 @@ namespace DHGCDB.Templates
       {
         "ADDRESS_POSTCODE", (Review review) => {
           return review.Client.Address.PostCode;
+        }
+      },
+      {
+        "REPORT_DATE", (Review review) => {
+          return String.Format("{0}{1} {2} {3}",
+            review.ReviewDate.Day,
+            GetSuffix(review.ReviewDate.Day),
+            review.ReviewDate.ToString("M"),
+            review.ReviewDate.Year);
         }
       },
     };
